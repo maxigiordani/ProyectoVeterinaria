009b0750016a880142efd31daa9e9de71e768b72
agusmena <agusmena60@mail.com>
2023-12-01 12:00:35 -0300

    all good

--- src/components/helpers/validateFieldCreate.jsx
+++ src/components/helpers/validateFieldCreate.jsx
@@ -1,4 +1,4 @@
-const regExpName = /^[A-Za-z\s?]+$/;
+ const regExpName = /^[A-Za-z\s?]+$/ ;
 const regExpEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
 const regExpTel = /^\d{12}$/
 const regExpTime =  /^(?:1[0-6]|[89])$/;
@@ -34,3 +34,86 @@ export const validateTime = (field)=>{
         false
     }
 }
+ 
+
+
+/* 
+export const validationsFieldCreate ={
+
+ownerName:{
+    requered: 'Este campo es obligatorio',
+    pattern:{
+        value: /^[A-Za-z\s?]+$/,
+        message: 'El nombre no puede contener espacios en blancos(ni al principio, ni al final), tampoco números'
+    },
+    maxLength:{
+        value:30,
+        message: 'El nombre no puede tener más de 25 caracteres',
+
+    }
+},
+email: {
+    required: 'Este campo es obligatorio',
+    pattern: { 
+      value:  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ,
+      message: 'Correo electrónico inválido',
+    },
+    maxLength: {
+      value: 30,
+      message: 'El correo electrónico no puede tener más de 35 caracteres',
+    },
+  },
+  tel:{
+    required: 'Este campo es obligatorio',
+    pattern: {
+      value: /^\d{12}$/,
+      message: 'Número de celular inválido. Debe tener 12 dígitos.',
+    },
+  },
+  petName:{
+    requered: 'Este campo es obligatorio',
+    pattern:{
+        value: /^[A-Za-z\s?]+$/,
+        message: 'El nombre de la mascota no puede contener espacios en blancos(ni al principio, ni al final), tampoco números'
+    },
+    maxLength:{
+        value:30,
+        message: 'El nombre de la mascota no puede tener más de 25 caracteres',
+
+    }
+},
+specie:{
+    requered: 'Este campo es obligatorio',
+    pattern:{
+        value: /^[A-Za-z\s?]+$/,
+        message: 'El nombre no puede contener espacios en blancos(ni al principio, ni al final), tampoco números'
+    },
+    maxLength:{
+        value:30,
+        message: 'El nombre no puede tener más de 25 caracteres',
+
+    }
+},
+race:{
+    requered: 'Este campo es obligatorio',
+    pattern:{
+        value: /^[A-Za-z\s?]+$/,
+        message: 'El nombre no puede contener espacios en blancos(ni al principio, ni al final), tampoco números'
+    },
+    maxLength:{
+        value:30,
+        message: 'El nombre no puede tener más de 25 caracteres',
+
+    }
+}
+
+};
+export const errorStyle = {
+    color: 'red',
+    fontSize: '14px',
+  };
+  
+  export const ErrorMessage = ({ message }) => (
+    <p style={errorStyle}>{message}</p>
+  );
+   */
