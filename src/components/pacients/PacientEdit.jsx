4065ae66107c527b3b5626c2b95cff3dd6963195
agusmena <agusmena60@mail.com>
2023-12-02 20:09:29 -0300

    add validation edit pacient

--- src/components/pacients/PacientEdit.jsx
+++ src/components/pacients/PacientEdit.jsx
@@ -1,19 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-import { useEffect, useRef, useState } from 'react';
-import { Container, Form, Alert } from 'react-bootstrap';
-import { useNavigate, useParams } from 'react-router-dom';
-import axios from '../config/axiosInit';
-import Swal from 'sweetalert2';
-import { STATUS } from '../constant/index';
-import { useForm } from 'react-hook-form';
-import { ErrorMessage, validationsFieldCreate } from '../helpers/validateFieldCreate';
+import { useEffect, useState } from "react";
+import { Form, Container, Alert } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "../config/axiosInit";
+import Swal from "sweetalert2";
+import { STATUS } from "../constant";
+import { useForm } from "react-hook-form";
+import { ErrorMessage, validationsFieldCreate } from "../helpers/validateFieldCreate";
 
-const PacientEdit = ({ getAPI }) => {
+const PacientEdit = ({ getTurnosAPI }) => {
   const {
     register,
     handleSubmit,
-    setValue,
     formState: { errors },
+    setValue,
     reset,
   } = useForm();
 
@@ -21,13 +22,6 @@ const PacientEdit = ({ getAPI }) => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [errorMessage, setErrorMessage] = useState(null);
-
-  const pacientOwnerNameRef = useRef(null);
-  const pacientEmailRef = useRef(null);
-  const pacientTelRef = useRef(null);
-  const pacientPetNameRef = useRef(null);
-  const pacientSpecieRef = useRef(null);
-  const pacientRaceRef = useRef(null);
 
   useEffect(() => {
     getOne();
@@ -37,15 +31,13 @@ const PacientEdit = ({ getAPI }) => {
   const getOne = async () => {
     try {
       const res = await axios.get(`${URL}/${id}`);
-      const pacientApi = res.data;
+      const pacientAPI = res.data;
 
-      // Setear los valores al formulario
-      Object.entries(pacientApi).forEach(([key, value]) => {
+      Object.entries(pacientAPI).forEach(([key, value]) => {
         setValue(key, value);
       });
     } catch (error) {
       console.log(error);
-      // Manejo de errores aquí, puedes mostrar un mensaje o redirigir a otra página
     }
   };
 
@@ -55,9 +47,9 @@ const PacientEdit = ({ getAPI }) => {
 
       console.log(res);
       if (res.status === STATUS.STATUS_OK) {
-        Swal.fire('Updated', 'Your patient has been updated successfully', 'success');
-        reset(); // Resetea el formulario
-        getAPI();
+        Swal.fire('Updated', 'Your pacient has been updated successfully', 'success');
+        reset(); 
+        getTurnosAPI();
         navigate('/admin/pacientes');
       }
     } catch (error) {
@@ -74,87 +66,74 @@ const PacientEdit = ({ getAPI }) => {
   return (
     <div>
       <Container className="py-5">
-        <h1>Edit Patient</h1>
+        <h1>Edit Pacient 🐾</h1>
         <hr />
-        {/* Form Patient */}
         <Form className="my-5" onSubmit={handleSubmit(onSubmit)}>
-          <Form.Group className="mb-3" controlId="formOwnerName">
-            <Form.Label>Owner name*</Form.Label>
+          <Form.Group className="mb-3" controlId="formDetails">
+            <Form.Label>Owner Name*</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Ej: Pepe Argento"
-              name="ownerName"
-              ref={(e) => {
-                pacientOwnerNameRef.current = e;
-                register(e, validationsFieldCreate.ownerName);
-              }}
+              {...register("ownerName", validationsFieldCreate.ownerName)}
             />
             {errors.ownerName && (
               <ErrorMessage message={errors.ownerName.message} />
             )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formEmail">
-            <Form.Label>Email*</Form.Label>
+          <Form.Group className="mb-3" controlId="formVeterinarian">
+            <Form.Label>email*</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Ej: pepeargento@gmail.com"
-              ref={(e) => {
-                pacientEmailRef.current = e;
-                register(e, validationsFieldCreate.email);
-              }}
+              {...register("email", validationsFieldCreate.email)}
             />
-            {errors.email && <ErrorMessage message={errors.email.message} />}
+            {errors.email && (
+              <ErrorMessage message={errors.email.message} />
+            )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formTel">
-            <Form.Label>Tel *</Form.Label>
+          <Form.Group className="mb-3" controlId="formtel">
+            <Form.Label>Tel*</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Ej: 38156334051"
-              ref={(e) => {
-                pacientTelRef.current = e;
-                register(e, validationsFieldCreate.tel);
-              }}
+              placeholder="E.g., Max"
+              {...register("tel", validationsFieldCreate.tel)}
             />
-            {errors.tel && <ErrorMessage message={errors.tel.message} />}
+            {errors.tel && (
+              <ErrorMessage message={errors.tel.message} />
+            )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formPetName">
-            <Form.Label>Pet Name*</Form.Label>
+          <Form.Group className="mb-3" controlId="formDate">
+            <Form.Label>petName*</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Ej: Coco"
-              ref={(e) => {
-                pacientPetNameRef.current = e;
-                register(e, validationsFieldCreate.petName);
-              }}
+              placeholder="Buddy"
+              {...register("petName", validationsFieldCreate.petName)}
             />
             {errors.petName && (
               <ErrorMessage message={errors.petName.message} />
             )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formSpecie">
+          <Form.Group className="mb-3" controlId="formTime">
             <Form.Label>Specie*</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Ej: Perro"
-              ref={(e) => {
-                pacientSpecieRef.current = e;
-                register(e, validationsFieldCreate.specie);
-              }}
+              placeholder="Perro"
+              {...register("specie", validationsFieldCreate.specie)}
             />
-            {errors.specie && <ErrorMessage message={errors.specie.message} />}
+            {errors.specie && (
+              <ErrorMessage message={errors.specie.message} />
+            )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formR">
+          <Form.Group className="mb-3" controlId="formTime">
             <Form.Label>Race*</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Ej: Bulldog Frances"
-              ref={(e) => {
-                pacientRaceRef.current = e;
-                register(e, validationsFieldCreate.race);
-              }}
+              placeholder="Labrador"
+              {...register("race", validationsFieldCreate.race)}
             />
-            {errors.race && <ErrorMessage message={errors.race.message} />}
+            {errors.race && (
+              <ErrorMessage message={errors.race.message} />
+            )}
           </Form.Group>
+
           <div className="text-end">
             <button className="btn-violeta">Update</button>
           </div>
