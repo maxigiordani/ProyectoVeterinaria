5e0bc2b56c7db537932d44744dbb61e6bb044d2f
maxigiordani <maxigiordani05@gmail.com>
2023-12-03 13:22:08 -0300

    update in nav

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,25 +1,15 @@
 
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '/src/App.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="social-media">
-        {/* Agrega tus iconos o enlaces de redes sociales aquí */}
-        <a href="#" target="_blank" rel="noopener noreferrer">Facebook</a>
-        <a href="#" target="_blank" rel="noopener noreferrer">Twitter</a>
-        {/* Agrega más redes sociales según sea necesario */}
-      </div>
-      <div className="contact-info">
-        <p>Ubicación: Tu dirección</p>
-        <p>Teléfono: Tu número de teléfono</p>
-        {/* Agrega más información de contacto según sea necesario */}
-      </div>
-      <div className="extra-info">
-        {/* Agrega información adicional según sea necesario */}
-      </div>
-    </footer>
+    <footer className="stylefooter bg-dark text-light">
+
+      
+  
+
+  </footer>
   );
 };
 
