677af41a98f2a618c5cb1b82b504eff4301e5340
agusmena <agusmena60@mail.com>
2023-12-04 09:27:03 -0300

    delete some comments

--- src/components/helpers/validateFieldCreate.jsx
+++ src/components/helpers/validateFieldCreate.jsx
@@ -1,44 +1,4 @@
-/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-/*  const regExpName = /^[A-Za-z\s?]+$/ ;
-const regExpEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
-const regExpTel = /^\d{12}$/
-const regExpTime =  /^(?:1[0-6]|[89])$/;
-
-
-export const validateNames=(field)=>{
-    if(regExpName.test(field)&& field.trim() !== "") {
-        return true;
-    }else{
-        return false;
-    }
-}
-
-export const validateEmail=(field)=>{
-    if(regExpEmail.test(field)&& field.trim() !== "") {
-        return true;
-    }else{
-        return false;
-    }
-}
-export const validateTel=(field)=>{
-    if(regExpTel.test(field)&& field.trim() !== "") {
-        return true;
-    }else{
-        return false;
-    }
-}
-
-export const validateTime = (field)=>{
-    if(regExpTime.test(field)&& field.trim()!== ""){
-        return true;
-    }else{
-        false
-    }
-}
-  */
-
-
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const validationsFieldCreate ={
@@ -111,6 +71,7 @@ race:{
 }
 
 };
+// eslint-disable-next-line react-refresh/only-export-components
 export const validationAppointment={
     appointmentDetail:{
         required: 'Este campo es obligatorio',
