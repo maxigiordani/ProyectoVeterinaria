abffd10185ba6de750267da918e8c648745359cd
maxigiordani <maxigiordani05@gmail.com>
2023-12-04 11:38:34 -0300

    updates styles in footer

--- src/components/helpers/Credintals.jsx
+++ src/components/helpers/Credintals.jsx
@@ -1,20 +1,16 @@
-// Importa la librería bcryptjs para encriptar contraseñas
+
 import bcrypt from 'bcryptjs';
 
-// Define la cantidad de "salt rounds" o rondas de sal. Cuanto mayor sea este número, más seguro pero más lento será el proceso.
 const saltRounds = 10;
 
-// Obtiene la contraseña del administrador desde la variable de entorno
 const adminPassword = import.meta.env.VITE_ADMIN_PASSWORD 
 
-// Genera un hash (contraseña encriptada) utilizando bcrypt y el número de salt rounds
 const adminPasswordHash = bcrypt.hashSync(adminPassword, saltRounds);
 
-// Objeto que contiene las credenciales del administrador
+
 const admin = {
   username: 'admin',
   passwordHash: adminPasswordHash,
 };
 
-// Exporta el objeto de credenciales
 export { admin };
--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,22 +1,54 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '/src/App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import logo from '../../assets/imagenes/logo1.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="contact-info">
-          <p>Ubicación: Tu dirección aquí</p>
-          <p>Teléfono: Tu número de teléfono aquí</p>
-        </div>
-        <div className="social-media">
-          <a href="#" target="_blank" rel="noopener noreferrer">Facebook</a>
-          <a href="#" target="_blank" rel="noopener noreferrer">Twitter</a>
-          <a href="#" target="_blank" rel="noopener noreferrer">Instagram</a>
-          {/* Agrega más redes sociales según sea necesario */}
-        </div>
-      </div>
+      <Container className='text-center'>
+        <Row className="justify-content-center">
+          {/* Primera columna con el logo y derechos reservados */}
+          <Col md={6} lg={3}>
+            <img
+              src={logo}
+              alt="Logo de la Empresa"
+              className="d-inline-block align-top logofooter"
+            />
+            <p className=" mb-2">© 2023 Todos los derechos reservados</p>
+          </Col>
+
+          {/* Segunda columna con enlaces "Nosotros" */}
+          <Col md={6} lg={3}>
+            <h5>Nosotros</h5>
+            <ul className="list-unstyled">
+              <li><a className='linkfooter' href="/">INICIO</a></li>
+              <li><a className='linkfooter' href="/aboutus">ACERCA DE NOSOTROS</a></li>
+              <li><a className='linkfooter' href="/contactus">CONTÁCTENOS</a></li>
+            </ul>
+          </Col>
+
+          {/* Tercera columna con enlaces a redes sociales */}
+          <Col md={6} lg={3} >
+            <h5>Redes Sociales</h5>
+            <ul className="list-unstyled">
+              <li><a className='linkfooter' href="https://facebook.com">FACEBOOK</a></li>
+              <li><a className='linkfooter' href="https://instagram.com">INSTAGRAM</a></li>
+              <li><a className='linkfooter' href="https://twitter.com">X</a></li>
+            </ul>
+          </Col>
+
+          {/* Cuarta columna de contacto */}
+          <Col md={6} lg={3} >
+            <h5>Contacto</h5>
+            <ul className="list-unstyled">
+              <li className='linkfooter'  >TELEFÓNO: (381) 456-7850</li>
+              <li className='linkfooter'>EMAIL: veternariarolling23@gmail.com</li>
+              <li className='linkfooter'>WHATSAPP: +5493814238444</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
