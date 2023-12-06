84cc40a85abe5e1c41cba372aafd1125e77a6192
maxigiordani <maxigiordani05@gmail.com>
2023-12-06 14:49:53 -0300

    route problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import PageAdmin from './components/pages/PageAdmin';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/app.css';
 import axios from "../src/components/config/axiosInit";
 import PacientEdit from './components/pacients/PacientEdit'
 import PacientTable from './components/pacients/PacientTable';
