a7d9f23d5d3fe4f9a22a982f5d4933c4d3eb15d8
maxigiordani <maxigiordani05@gmail.com>
2023-12-07 13:37:38 -0300

    add logic in nav

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/imagenes/logouno.png";
 
-const NavbarComponent = ({ isAdmin }) => {
+const NavbarComponent = () => {
   const location = useLocation();
+  const [isAdminPage, setIsAdminPage] = useState(false);
+
+  useEffect(() => {
+    setIsAdminPage(
+      location.pathname.startsWith("/admin") ||
+        location.pathname === "/pageadmin" ||
+        location.pathname === "/pacient/create" ||
+        location.pathname === "/turnos/create"
+    );
+  }, [location.pathname]);
 
   const isSmallScreen = () => window.innerWidth <= 768;
 
@@ -23,31 +33,26 @@ const NavbarComponent = ({ isAdmin }) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="botonnav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {isAdmin && !isSmallScreen() && ( 
-              (location.pathname.startsWith("/admin") ||
-                location.pathname === "/pageadmin" ||
-                location.pathname === "/pacient/create" ||
-                location.pathname === "/turnos/create") && (
-                <>
-                  <Nav.Link className="linkadm" as={Link} to="pageadmin">
-                    ADMINISTRADOR
-                  </Nav.Link>
-                  <Nav.Link className="linkadm" as={Link} to="admin/pacientes">
-                    ADMINISTRAR PACIENTES
-                  </Nav.Link>
-                  <Nav.Link className="linkadm" as={Link} to="admin/turnos">
-                    ADMINISTRAR TURNOS
-                  </Nav.Link>
-                </>
-              )
+            {isAdminPage && !isSmallScreen() && (
+              <>
+                <Nav.Link className="linkadm" as={Link} to="pageadmin">
+                  ADMINISTRADOR
+                </Nav.Link>
+                <Nav.Link className="linkadm" as={Link} to="admin/pacientes">
+                  ADMINISTRAR PACIENTES
+                </Nav.Link>
+                <Nav.Link className="linkadm" as={Link} to="admin/turnos">
+                  ADMINISTRAR TURNOS
+                </Nav.Link>
+              </>
             )}
           </Nav>
           <Nav className="ms-auto">
             <Nav.Link className="linknav" as={Link} to="/">
               INICIO
             </Nav.Link>
-            <Nav.Link className="linknav" as={Link} to="/pagelogin">
-              INGRESAR
+            <Nav.Link className="linknav" as={Link} to={isAdminPage ? "/" : "/pagelogin"}>
+              {isAdminPage ? "SALIR" : "INGRESAR"}
             </Nav.Link>
             <Nav.Link className="linknav" as={Link} to="/contactus">
               CONTÁCTENOS
